b422c9f3f5fae2de1a8e26a954a911725fcc5a63
Marco Paolo Valerio Vezzoli <marco.p.v.vezzoli@gmail.com>
2024-03-22 17:00:50 +0100

    added tests and errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,128 @@
-pub struct BStack0 {
+
+pub struct Stack0 {}
+
+impl Stack0 {
+    pub fn new() -> Stack0 {
+        Stack0 {}
+    }
+
+    pub fn push(self: & mut Stack0, value: bool) {
+    }
+
+    pub fn top(self: & Stack0) -> bool {
+        true
+    }
+
+    pub fn pop(self: & mut Stack0) -> bool {
+        true
+    }
+
+    pub fn size(self: & Stack0) -> u32 {
+        1
+    }
+}
+
+pub struct Stack1 {
     stack: usize
 }
 
-impl BStack0{
-    pub fn new() -> BStack0 {
-        BStack0 { stack: 1 }
+impl Stack1 {
+    pub fn new() -> Stack1 {
+        Stack1 { stack: 1 }
     }
 
-    pub fn pop(self : & mut BStack0) -> Result<usize, str> {
-        Ok(0)
+    pub fn push(& mut self, value: bool) -> Result<bool, String>{
+        Ok(value)
     }
 
-    fn size(self : & BStack0) -> u32 {
-        //what is the first
-        usize::BITS - usize::leading_zeros(self.stack) - 1
+    pub fn top(& self) -> Result<bool, String> {
+        Ok(true)
     }
 
-    fn is_empty(self : & BStack0) -> bool {
-        self.stack == 1
+    pub fn pop(& mut self) -> Result<bool, String> {
+        Ok(true)
+    }
+
+    pub fn size(& self) -> u32 {
+        1
     }
 }
+// impl BStack0{
+//     pub fn new() -> BStack0 {
+//         BStack0 { stack: 1 }
+//     }
+
+//     pub fn pop(self : & mut BStack0) -> Result<usize, str> {
+//         Ok(0)
+//     }
+
+//     fn size(self : & BStack0) -> u32 {
+//         //what is the first
+//         usize::BITS - usize::leading_zeros(self.stack) - 1
+//     }
+
+//     fn is_empty(self : & BStack0) -> bool {
+//         self.stack == 1
+//     }
+// }
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn empty_when_created() {
-        let result = BStack0::new().is_empty();
-        assert_eq!(result, true);
+    fn push_and_pop() {
+        let mut stack = Stack1::new();
+        stack.push(false);
+        stack.push(true);
+        let result1 = stack.pop();
+        let result2 = stack.pop();
+        assert_eq!(result1, Ok(true));
+        assert_eq!(result2, Ok(false));
     }
 
     #[test]
-    fn size_0_when_created() {
-        let result = BStack0::new().size();
+    fn size_increase_when_push() {
+        let mut stack = Stack1::new();
+        stack.push(false);
+        let result1 = stack.size();
+        stack.push(true);
+        let result2 = stack.size();
+        assert_eq!(result2, result1 + 1);
+    }
+
+    #[test]
+    fn push_and_top() {
+        let mut stack = Stack1::new();
+        stack.push(false);
+        let result1 = stack.top();
+        stack.push(true);
+        let result2 = stack.top();
+        assert_eq!(result1, Ok(false));
+        assert_eq!(result2, Ok(true));
+    }
+
+    #[test]
+    fn size_decreases_when_pop() {
+        let mut stack = Stack1::new();
+        stack.push(false);
+        stack.push(true);
+        let result1 = stack.size();
+        stack.pop();
+        let result2 = stack.size();
+        assert_eq!(result1, result2 - 1);
+    }
+
+    #[test]
+    fn empty_when_created() {
+        let result = Stack1::new().size();
         assert_eq!(result, 0);
     }
+
+    #[test]
+    fn empty_does_not_pop() {
+        let mut stack = Stack1::new();
+        let result = stack.pop();
+        assert_eq!(result, Err("Empty stack".into()));
+    }
+
 }
